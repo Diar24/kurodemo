7628d38a195b9c6ccc9c1862322434043a2abf0e
Diar24 <132535544+Diar24@users.noreply.github.com>
2024-10-04 13:32:47 +0700

    Create index.php
    template

---
+++ index.php
@@ -0,0 +1,176 @@
+<!doctype html>
+<html lang="en" data-bs-theme="auto">
+  <head><script src="assets/js/color-modes.js"></script>
+
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <meta name="description" content="">
+    <meta name="author" content="Mark Otto, Jacob Thornton, and Bootstrap contributors">
+    <meta name="generator" content="Hugo 0.122.0">
+    <title>Kuro Production</title>
+
+    <link rel="canonical" href="https://getbootstrap.com/docs/5.3/examples/features/">
+
+    
+
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/@docsearch/css@3">
+
+<link href="assets/dist/css/bootstrap.min.css" rel="stylesheet">
+
+    <style>
+      .bd-placeholder-img {
+        font-size: 1.125rem;
+        text-anchor: middle;
+        -webkit-user-select: none;
+        -moz-user-select: none;
+        user-select: none;
+      }
+
+      @media (min-width: 768px) {
+        .bd-placeholder-img-lg {
+          font-size: 3.5rem;
+        }
+      }
+
+      .b-example-divider {
+        width: 100%;
+        height: 3rem;
+        background-color: rgba(0, 0, 0, .1);
+        border: solid rgba(0, 0, 0, .15);
+        border-width: 1px 0;
+        box-shadow: inset 0 .5em 1.5em rgba(0, 0, 0, .1), inset 0 .125em .5em rgba(0, 0, 0, .15);
+      }
+
+      .b-example-vr {
+        flex-shrink: 0;
+        width: 1.5rem;
+        height: 100vh;
+      }
+
+      .bi {
+        vertical-align: -.125em;
+        fill: currentColor;
+      }
+
+      .nav-scroller {
+        position: relative;
+        z-index: 2;
+        height: 2.75rem;
+        overflow-y: hidden;
+      }
+
+      .nav-scroller .nav {
+        display: flex;
+        flex-wrap: nowrap;
+        padding-bottom: 1rem;
+        margin-top: -1px;
+        overflow-x: auto;
+        text-align: center;
+        white-space: nowrap;
+        -webkit-overflow-scrolling: touch;
+      }
+
+      .btn-bd-primary {
+        --bd-violet-bg: #712cf9;
+        --bd-violet-rgb: 112.520718, 44.062154, 249.437846;
+
+        --bs-btn-font-weight: 600;
+        --bs-btn-color: var(--bs-white);
+        --bs-btn-bg: var(--bd-violet-bg);
+        --bs-btn-border-color: var(--bd-violet-bg);
+        --bs-btn-hover-color: var(--bs-white);
+        --bs-btn-hover-bg: #6528e0;
+        --bs-btn-hover-border-color: #6528e0;
+        --bs-btn-focus-shadow-rgb: var(--bd-violet-rgb);
+        --bs-btn-active-color: var(--bs-btn-hover-color);
+        --bs-btn-active-bg: #5a23c8;
+        --bs-btn-active-border-color: #5a23c8;
+      }
+
+      .bd-mode-toggle {
+        z-index: 1500;
+      }
+
+      .bd-mode-toggle .dropdown-menu .active .bi {
+        display: block !important;
+      }
+    </style>
+
+    
+    <!-- Custom styles for this template -->
+    <link href="features.css" rel="stylesheet">
+  </head>
+  <body>
+     
+
+  
+    
+
+<main>
+  <h1 class="visually-hidden">Features examples</h1>
+
+  <div class="container px-4 py-5" id="featured-3">
+
+    <img src="assets/img/kuro.png" alt="Bootstrap" width="200" height="100" align="center">
+    <h2 class="pb-2 border-bottom"></h2>
+    <div class="row g-4 py-5 row-cols-1 row-cols-lg-1">
+      <div class="feature col">
+        <h3 class="fs-2 text-body-emphasis">KR001</h3>
+        <img src="assets/img/kuro1.png" class="img-fluid">
+          <p>Pre-order schedule :</br>
+            • Pendaftaran preorder mulai 21 September – 16 Oktober</br>
+            • Pembayaran Maksimal 15 Oktober (minim depe 50%)</br>
+            • Start produksi 18 Oktober</br>
+            • Pengiriman bertahap setelah produksi selesai</br>
+            • Pelunasan maksimal 15 November </br>
+            Promo :</br>
+
+            • Discon 10K untuk 50 pemesan pertama</br>
+            • Free Keychain untuk pembelian minim 3 item
+        </p>
+         </div>
+       </div>
+      </div>
+  <div class="b-example-divider"></div>
+<h1 class="visually-hidden">Features examples</h1>
+
+  <div class="container px-4 py-5" id="featured-3">
+      <div class="feature col">
+        <h3 class="fs-2 text-body-emphasis">KR002</h3>
+        <img src="assets/img/kuro2.jpg" class="img-fluid">
+          <p>Pre-order schedule :</br>
+            • Pendaftaran preorder mulai 21 September – 16 Oktober</br>
+            • Pembayaran Maksimal 15 Oktober (minim depe 50%)</br>
+            • Start produksi 18 Oktober</br>
+            • Pengiriman bertahap setelah produksi selesai</br>
+            • Pelunasan maksimal 15 November </br>
+            Promo :</br>
+
+            • Discon 10K untuk 50 pemesan pertama</br>
+            • Free Keychain untuk pembelian minim 3 item
+        </p>
+         </div>
+       </div>
+      </div>
+  <div class="b-example-divider"></div>
+
+
+  <div class="container px-4 py-5">
+    <h2 class="pb-2 border-bottom">Untuk pendaftaran preorder bisa klik link Whatsapp dibawah ini</h2>
+
+    <div class="row row-cols-1 row-cols-md-2 align-items-md-center g-5 py-5">
+      <div class="col d-flex flex-column align-items-start gap-2">
+        <h2 class="fw-bold text-body-emphasis">Left-aligned title explaining these awesome features</h2>
+        <p class="text-body-secondary">Paragraph of text beneath the heading to explain the heading. We'll add onto it with another sentence and probably just keep going until we run out of words.</p>
+        <a href="#" class="btn btn-primary btn-lg">Primary button</a>
+      </div>
+
+     
+
+    </div>
+  </div>
+</main>
+<script src="../assets/dist/js/bootstrap.bundle.min.js"></script>
+
+    </body>
+</html>
